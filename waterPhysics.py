8b642e445cffbf91d6f871819ab0ab77f293b679
yarnoiser <yarnoiserdev@gmail.com>
2016-12-12 01:13:19 -0700

    Initial Commit

---
+++ waterPhysics.py
@@ -0,0 +1,35 @@
+import bge
+
+GRAVITY = 9.8
+FRICTION = 7
+
+def hasWaterPhysics(object):
+    try:
+        object['volume']
+    except:
+        return False
+    return True
+
+fluid = bge.logic.getCurrentController().owner
+
+def submerged(object):
+    fluidZ = fluid.position[2]
+    objectZ = object.position[2]
+    return (objectZ <= fluidZ)
+
+scene = bge.logic.getCurrentScene()
+
+for object in scene.objects:
+    if hasWaterPhysics(object):
+        if submerged(object):
+            # Buoyancy force
+            object.applyForce([0.0, 0.0, object['volume'] * fluid['density'] * GRAVITY],
+                              False)
+            
+            # Friction force
+            FrictionVect = object.worldLinearVelocity.copy()
+            FrictionVect.negate()
+            FrictionVect.normalize()
+            object.applyForce(FrictionVect * FRICTION, False)
+                             
+        
