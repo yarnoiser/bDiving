1adf879487e682159f01a5d44b3a1d1ed2588634
Robert Smiley <yarnoiserdev@gmail.com>
2016-12-31 19:17:53 -0700

    player sinks by default

--- waterPhysics.py
+++ waterPhysics.py
@@ -1,30 +1,35 @@
 import bge
 import bpy
 
-FRICTION = 7
+FRICTION = 0
 
 scene = bge.logic.getCurrentScene()
 
-worldConfig = scene['WorldConfig']
-GRAVITY = worldConfig['GRAVITY']
-
 fluid = bge.logic.getCurrentController().owner
+
+GRAVITY = 9.81
+
+def hasWaterPhysics(obj):
+    return 'volume' in obj
+
+def airCompression(depth):
+    return 1 + (depth * 0.1) * fuid['density']
 
 def submerged(object):
     fluidZ = fluid.position[2]
     objectZ = object.position[2]
     return (objectZ <= fluidZ)
 
-for object in bpy.data.groups['waterPhysics']:
-    if submerged(object):
-    # Buoyancy force
-    object.applyForce([0.0, 0.0, object['volume'] * fluid['density'] * GRAVITY],
-                      False)
+for object in scene.objects:
+    if hasWaterPhysics(object):
+        if submerged(object):
+            # Buoyancy force
+            object.applyForce([0.0, 0.0, object['volume'] * fluid['density'] * GRAVITY],
+                          False)
             
-    # Friction force
-    FrictionVect = object.worldLinearVelocity.copy()
-    FrictionVect.negate()
-    FrictionVect.normalize()
-    object.applyForce(FrictionVect * FRICTION, False)
+            # Friction force
+            FrictionVect = object.worldLinearVelocity.copy()
+            FrictionVect.negate()
+            FrictionVect.normalize()
+            object.applyForce(FrictionVect * FRICTION, False)
                              
-        
